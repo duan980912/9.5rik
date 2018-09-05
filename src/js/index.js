af8e34582af72323070ae7c1045378271eb435c3
duan <duan980912@163.com>
2018-09-05 11:14:34 +0800

    finish

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
 let lis = document.querySelectorAll("header ul li");
-lis[0].classList.add("active")
+let box = document.querySelectorAll("section .content");
+let lisArr = Array.from(lis)
+lis[0].classList.add("active");
+box[0].style.display = "block";
+
+
+function hideBox() {
+    for (let i = 0; i < box.length; i++) {
+        box[i].style.display = "none";
+    }
+}
+
+function removeClass() {
+    for (let i = 0; i < lis.length; i++) {
+        lis[i].classList.remove("active")
+    }
+}
+lisArr.map(function(file, index) {
+    file.onclick = function() {
+        removeClass();
+        hideBox();
+        lis[index].classList.add("active");
+        box[index].style.display = "block";
+    }
+})
